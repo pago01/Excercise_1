b0905556bcf7edd4c1eeadd6c46b4e32f42cee6b
ACTFYZZ <ACTFYZZ@PL10006677>
2023-08-07 00:16:02 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+namespace Helloworld
+{ 
+    //PascalCase [klasy, pliki, namespace, metody]
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //camelCase [lokalne zmienne, nipubliczne elementy programu]
+            string imie = "Pablo";
+
+            //UPPER_CASE
+            const double PI_NUMBER = 3.14;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Yo");
+            if (PI_NUMBER > 2)
+            {
+            }
+        }
+        private string WritePiNumber(double pi)
+        { 
+            return ("3.14");
+        }
+    }
+
+}
+
+public interface Iprogram
+    { }
